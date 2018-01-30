baec3ab492cbabd94c4f86e909cd43587610b07d
wenchichou <wenchichou@users.noreply.github.com>
2018-01-30 10:20:56 -0500

    Update GWAS.TissueSpecific.Function.Enrichment.sh

--- scripts/GWAS.TissueSpecific.Function.Enrichment.sh
+++ scripts/GWAS.TissueSpecific.Function.Enrichment.sh
@@ -2,26 +2,29 @@
 # Tissue-Specific Functional Enrichment Analysis for GWAS hits
 # This pipeline use HaploReg's ENCODE (ChromHMM) annotation to performe hypergeometric test for each SNP using its SNPs in LC block
 
-use Python-2.7
+# use Python-2.7
 # required python packages: bs4 
+# use R-3.3
 
 # enviroment setup
+repoDIR=`pwd`
 #resultDIR=$1 # where you store the intermediate results 
-resultDIR="./results/enrichment_grepStrenth"
+resultDIR="${repoDIR}/results/enrichment_leanMass"
 #inputSNPList=$2 # the list of input SNPs
-inputSNPList="./data/SNPs_grepStrength.txt"
+inputSNPList="${repoDIR}/data/SNPs_leanMass.txt"
 #collectSNPinLD_Py=$3 # a web parser to get SNPs in LD block by a given SNP
-collectSNPinLD_Py="./scripts/collectSNPinLD.py"
+collectSNPinLD_Py="${repoDIR}/scripts/collectSNPinLD.py"
 #collectChromHMM_Sh=$4 # a web parser to collect ChromHMM annotations
-collectChromHMM_Sh="./scripts/collectChromHMM.sh"
+collectChromHMM_Sh="${repoDIR}/scripts/collectChromHMM.sh"
 #enrichmentTest_R=$5 # an R script to run enrichmet analysis
-enrichmentTest_R="./scripts/enrichmentTest.TissueSpecific.GWAS.r"
+enrichmentTest_R="${repoDIR}/scripts/enrichmentTest.TissueSpecific.GWAS.r"
 #enrichmentTestFun_R=$6 # an R script contains the functions for enrichment analysis
-enrichmentTestFun_R="./scripts/enrichmentTest.TissueSpecific.GWAS.function.r"
+enrichmentTestFun_R="${repoDIR}/scripts/enrichmentTest.TissueSpecific.GWAS.function.r"
 #cellTable=$7 # a table describing the categories of all cell types
-cellTable="./data/allCell.group.ID.description.txt"
+cellTable="${repoDIR}/data/allCell.group.ID.description.txt"
 
 # change dir to the result directory 
+mkdir -p ${resultDIR}
 cd ${resultDIR} 
 
 # 1) create folders for each input SNP
